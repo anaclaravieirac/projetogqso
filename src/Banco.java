a66761920faf63a32820cef25b525a35f8df857a
anaclaravieirac <acvc1@aluno.ifal.edu.br>
2021-05-02 13:12:49 -0300

    Métodos na classe criados

--- src/Banco.java
+++ src/Banco.java
@@ -4,4 +4,13 @@ public class Banco {
     public Banco(double saldo) {
         this.saldo = saldo;
     }
+    public void deposito(double valor){
+        this.saldo += valor;
+    }
+    public void saque(double valor){
+        this.saldo -= valor;
+    }
+    public double saldo(){
+        return this.saldo;
+    }
 }

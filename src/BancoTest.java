c591ee11f77cbe7cb62d60bd0f489819c7915b16
anaclaravieirac <acvc1@aluno.ifal.edu.br>
2021-05-02 14:25:01 -0300

    Criação teste

---
+++ src/BancoTest.java
@@ -0,0 +1,17 @@
+import org.testng.annotations.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BancoTest {
+    @Test
+    void deposito(){
+
+    }
+    @Test
+    void saque(){
+
+    }
+    @Test
+    void saldo(){
+
+    }
+}
